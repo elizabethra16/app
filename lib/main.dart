19d602a0e23cae0093acfa70366d9773438d75f7
elizabethra16 <elizabeth160102@gmail.com>
2023-09-28 13:32:37 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:app/loginscreen.dart';
+import 'package:flutter/material.dart';
+
+  
+void main() {
+  runApp(MyApp());
+}
+  
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
